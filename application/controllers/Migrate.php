a503cb19c3d0f0cd04dc66230eef3c49694d36f1
dhonstudio <doon13@gmail.com>
2022-06-04 22:20:53 +0700

    preparation for auto deploy on web

--- application/controllers/Migrate.php
+++ application/controllers/Migrate.php
@@ -3,39 +3,34 @@ date_default_timezone_set('Asia/Jakarta');
 
 class Migrate extends CI_Controller
 {
+    /*
+    | ------------------------------------------------------------------
+    |  Set up Migration Database, Method, File, and Version
+    | ------------------------------------------------------------------
+    */
+    protected $migration_database   = 'project';
+    protected $migration_method     = 'one'; // one | all
+    protected $migration_file       = 'api_users'; // same with migration file created
+    protected $migration_version    = '20220602143026'; // same with migration file created
+    protected $migration_action     = ''; // '' | 'change' | 'drop' | 'relate'
+    protected $dhonmigrate;
+
     public function __construct()
     {
         parent::__construct();
 
-        /*
-        | ------------------------------------------------------------------
-        |  Set up Migration Database, Method, File, and Version
-        | ------------------------------------------------------------------
-        */
-        $database                   = 'project';
-        $this->migration_method     = 'one'; // one | all
-        $this->migration_file       = 'api_users'; // same with migration file created
-        $this->migration_version    = '20220602143026'; // same with migration file created
-        $this->migration_action     = ''; // '' | 'change' | 'drop' | 'relate'
-
         require_once APPPATH . 'libraries/DhonMigrate.php';
-        $this->dhonmigrate = new DhonMigrate(['database' => $database]);
+        $this->dhonmigrate = new DhonMigrate(['database' => $this->migration_database]);
+        $this->dhonmigrate->version = $this->migration_version;
     }
 
     public function index()
     {
-        $this->dhonmigrate->version = $this->migration_version;
-        $this->migration_method == 'one' ? $this->dhonmigrate->migrate($this->migration_file, $this->migration_action) : '';
+        $this->migration_method == 'one' ? $this->dhonmigrate->migrate($this->migration_file, $this->migration_action) : false;
     }
 
     public function create(string $migration_name, string $dev = '')
     {
         $this->dhonmigrate->create($migration_name, $dev);
-    }
-
-    public function test()
-    {
-        $this->dhonmigrate->table = 'api_users';
-        $this->dhonmigrate->test();
     }
 }
